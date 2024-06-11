b716c15feca8758db91bf4f9c4a4e42f92dda93a
Matias A. Eguia <matias_eguia@hotmail.com>
2024-06-11 20:04:12 -0300

    Cambio de puerto 8080

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({whitelist:true}));
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(8080);
   
 }
 
